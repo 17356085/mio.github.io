f0f2d1e30ee55f2a3add7054c74bddc0fd921d4b
mio <1735608505@qq.com>
2025-08-12 11:48:52 +0800

    添加Github Actions自动部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://17356085/mio.github.io",
+	site: "https://mio.github.io",
 	base: "/mio_blog",
 	trailingSlash: "always",
 	integrations: [
