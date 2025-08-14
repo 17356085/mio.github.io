95ff94a264d41f4ae9d74ef242416428be6ddeac
mio <1735608505@qq.com>
2025-08-14 16:15:04 +0800

    指向的网址变更为自定义的域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://17356085.github.io",
+	site: "https://devmio.cc",
 	base: "",
 	trailingSlash: "always",
 	integrations: [
